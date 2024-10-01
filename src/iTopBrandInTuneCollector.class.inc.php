9ed3be8383b238b5d941c64f66ac0297a53fccd3
xtophe38 <christophe.naud@gmail.com>
2024-10-01 16:33:25 +0200

    Update Brand mgmt

--- src/iTopBrandInTuneCollector.class.inc.php
+++ src/iTopBrandInTuneCollector.class.inc.php
@@ -4,6 +4,16 @@ require_once(APPROOT.'collectors/msbase/src/MSJsonCollector.class.inc.php');
 class iTopBrandInTuneCollector extends MSJsonCollector
 {
     /**
+     * @inheritdoc
+     */
+    public function AttributeIsOptional($sAttCode)
+    {
+        if ($sAttCode == 'azuretags') return true;
+
+        return parent::AttributeIsOptional($sAttCode);
+    }
+
+    /**
 	 * @inheritdoc
 	 */
 	protected function BuildUrl($aParameters): string
