e14ed9640772f6cd58e0b3d44bd97dcfc35ff506
xtophe38 <christophe.naud@gmail.com>
2024-10-01 16:30:01 +0200

    Add PC, Tablet and Mobile Phone mgmt

---
+++ src/iTopMobilePhoneInTuneCollector.class.inc.php
@@ -0,0 +1,56 @@
+<?php
+
+class iTopMobilePhoneInTuneCollector extends JsonCollector
+{
+    private $oModelLookup;
+    private $oOSVersionLookup;
+
+    /**
+     * @inheritdoc
+     */
+    protected function MustProcessBeforeSynchro()
+    {
+        // We must reprocess the CSV data obtained from the inventory script
+        // to lookup the Brand/Model and OSFamily/OSVersion in iTop
+        return true;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function InitProcessBeforeSynchro()
+    {
+        $this->oModelLookup = new LookupTable('SELECT Model AS m WHERE m.type = \'MobilePhone\'', array('brand_id_friendlyname', 'name'));
+        $this->oOSVersionLookup = new LookupTable('SELECT OSVersion', array('osfamily_id_friendlyname', 'name'));
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function ProcessLineBeforeSynchro(&$aLineData, $iLineIndex)
+    {
+        if (!$this->oModelLookup->Lookup($aLineData, array('brand_id', 'model_id'), 'model_id', $iLineIndex))
+        {
+            throw New IgnoredRowException('Unknown Model');
+        }
+        if (!$this->oOSVersionLookup->Lookup($aLineData, array('osfamily_id', 'osversion_id'), 'osversion_id', $iLineIndex))
+        {
+            throw New IgnoredRowException('Unknown OS Version');
+        }
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function Fetch(): array | false
+    {
+        $aData = parent::Fetch();
+        if ($aData !== false) {
+             $aData['contacts_list'] = "contact_id->email:".$aData['contacts_list'];
+        }
+
+        return $aData;
+
+    }
+}
+
---
+++ src/iTopTabletInTuneCollector.class.inc.php
@@ -0,0 +1,50 @@
+<?php
+
+class iTopTabletInTuneCollector extends JsonCollector
+{
+    private $oModelLookup;
+
+    /**
+     * @inheritdoc
+     */
+    protected function MustProcessBeforeSynchro()
+    {
+        // We must reprocess the CSV data obtained from the inventory script
+        // to lookup the Brand/Model and OSFamily/OSVersion in iTop
+        return true;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function InitProcessBeforeSynchro()
+    {
+        $this->oModelLookup = new LookupTable('SELECT Model AS m WHERE m.type = \'Tablet\'', array('brand_id_friendlyname', 'name'));
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function ProcessLineBeforeSynchro(&$aLineData, $iLineIndex)
+    {
+        if (!$this->oModelLookup->Lookup($aLineData, array('brand_id', 'model_id'), 'model_id', $iLineIndex))
+        {
+            throw New IgnoredRowException('Unknown Model');
+        }
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function Fetch(): array | false
+    {
+        $aData = parent::Fetch();
+        if ($aData !== false) {
+            $aData['contacts_list'] = "contact_id->email:".$aData['contacts_list'];
+        }
+
+        return $aData;
+
+    }
+}
+
