89b7faffcd7baf0e468a12d6fad554b2a45e1817
xtophe38 <christophe.naud@gmail.com>
2024-11-12 16:51:54 +0100

    Keep history of collected OS versions to avoid duplicate entries in csv output

--- src/iTopOSVersionInTuneCollector.class.inc.php
+++ src/iTopOSVersionInTuneCollector.class.inc.php
@@ -2,19 +2,27 @@
 
 class iTopOSVersionInTuneCollector extends JsonCollector
 {
+    private $aCollectedOSVersions = [];
+
     /**
      * @inheritdoc
      */
     public function Fetch(): array | false
     {
+        // Exclude duplicate entries, if any
+        // Set type according to OSFamily / Model mapping defined in configuration file
         $aData = parent::Fetch();
-        if ($aData !== false) {
-            // Set primary_key
+        while ($aData !== false) {
             $aData['primary_key'] = $aData['osfamily_id'].'-'.$aData['name'];
+            if (($aData['osfamily_id'] != "") && !in_array($aData, $this->aCollectedOSVersions)) {
+                $this->aCollectedOSVersions[] = $aData;
+                break;
+            } else {
+                $this->iIdx++;
+                $aData = parent::Fetch();
+            }
         }
-
         return $aData;
-
     }
 
 }
