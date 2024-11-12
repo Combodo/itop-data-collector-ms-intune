6e164c91f1f0ef78091ce610ad08c6e7b33689d5
xtophe38 <christophe.naud@gmail.com>
2024-11-12 16:56:13 +0100

    Keep history of collected models to avoid duplicate entries in csv output and preset model type according to conf

--- src/iTopModelInTuneCollector.class.inc.php
+++ src/iTopModelInTuneCollector.class.inc.php
@@ -2,23 +2,45 @@
 
 class iTopModelInTuneCollector extends JsonCollector
 {
+    private $aCollectedModels = [];
+
     /**
      * @inheritdoc
      */
     public function Fetch(): array | false
     {
+        // Exclude duplicate entries, if any
+        // Set type according to OSFamily / Model mapping defined in configuration file
         $aData = parent::Fetch();
-        if ($aData !== false) {
-            $aData['primary_key'] = $aData['brand_id'].'-'.$aData['name'];
-            // Set type according to OSFamily / Model mapping
-            if (array_key_exists('osfamily_type_default_mapping', $this->aCollectorConfig)) {
-                $sType = $aData['type'];
-                if (array_key_exists($sType, $this->aCollectorConfig['osfamily_type_default_mapping'])) {
-                    $aData['type'] = $this->aCollectorConfig['osfamily_type_default_mapping'][$sType];
+        while ($aData !== false) {
+            $sBrand = $aData['brand_id'];
+            $aData['primary_key'] = $sBrand.'-'.$aData['name'];
+            if (($sBrand != "") && !in_array($aData, $this->aCollectedModels)) {
+                $this->aCollectedModels[] = $aData;
+                $sModelType = 'unknown';
+                if (array_key_exists('osfamily_type_default_mapping', $this->aCollectorConfig)) {
+                    $aDefaultMapping = $this->aCollectorConfig['osfamily_type_default_mapping'];
+                    $sOS = $aData['type'];
+                    if (array_key_exists($sOS, $aDefaultMapping)) {
+                        if (is_array($aDefaultMapping[$sOS])) {
+                            foreach ($aDefaultMapping[$sOS] AS $index => $aBrand) {
+                                if ($aBrand['name'] == $sBrand) {
+                                    $sModelType = $aBrand['type'];
+                                    break;
+                                }
+                            }
+                        } else {
+                            $sModelType = $aDefaultMapping[$sOS];
+                        }
+                    }
                 }
+                $aData['type'] = $sModelType;
+                break;
+            } else {
+                $this->iIdx++;
+                $aData = parent::Fetch();
             }
         }
-
         return $aData;
 
     }
