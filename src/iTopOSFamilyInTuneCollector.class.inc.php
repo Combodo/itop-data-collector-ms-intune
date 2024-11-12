d7537fc0580dbe6793f83d7f21cce459d393233a
xtophe38 <christophe.naud@gmail.com>
2024-11-12 16:50:09 +0100

    Keep history of collected OS families to avoid duplicate entries in csv output

--- src/iTopOSFamilyInTuneCollector.class.inc.php
+++ src/iTopOSFamilyInTuneCollector.class.inc.php
@@ -2,5 +2,25 @@
 
 class iTopOSFamilyInTuneCollector extends JsonCollector
 {
+    private $aCollectedOSFamilies = [];
+
+    /**
+     * @inheritdoc
+     */
+    public function Fetch() {
+        // Exclude duplicate entries, if any
+        $aData = parent::Fetch();
+        while ($aData !== false) {
+            if (($aData['primary_key'] != "") && !in_array($aData, $this->aCollectedOSFamilies)) {
+                $this->aCollectedOSFamilies[] = $aData;
+                break;
+            } else {
+                $this->iIdx++;
+                $aData = parent::Fetch();
+            }
+        }
+        return $aData;
+    }
+
 }
 
