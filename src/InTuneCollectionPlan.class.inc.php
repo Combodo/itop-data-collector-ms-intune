4602a2f52dcd8388eda0947c22bc12e40de9c432
xtophe38 <christophe.naud@gmail.com>
2024-12-04 15:03:41 +0100

    Collection plan checks if InTune DM is installed.

--- src/InTuneCollectionPlan.class.inc.php
+++ src/InTuneCollectionPlan.class.inc.php
@@ -3,7 +3,35 @@ require_once(APPROOT.'collectors/msbase/src/MSCollectionPlan.class.inc.php');
 
 class InTuneCollectionPlan extends MSCollectionPlan
 {
-	/**
+    private bool $bInTuneDatamodelIsInstalled;
+
+    /**
+     * @inheritdoc
+     */
+    public function Init(): void
+    {
+        parent::Init();
+
+        // Check if InTune Datamodel extension is installed
+        Utils::Log(LOG_INFO, '---------- Check InTune Datamodel extension installation ----------');
+        if ($this->bInTuneDatamodelIsInstalled = utils::CheckModuleInstallation('combodo-intune-datamodel')) {
+            Utils::Log(LOG_INFO, 'Extension InTune Datamodel has been installed on iTop');
+        } else {
+            Utils::Log(LOG_INFO, 'Extension InTune Datamodel has not been installed on iTop');
+        }
+    }
+
+    /**
+     * Check if Combodo InTune Datamodel is installed
+     *
+     * @return bool
+     */
+    public function IsCbdinTuneDMInstalled(): bool
+    {
+        return $this->bInTuneDatamodelIsInstalled;
+    }
+
+    /**
 	 * @inheritdoc
 	 */
 	public function AddCollectorsToOrchestrator(): bool
