03b0856652ba95680fdb7ad96586a177ecab35c6
xtophe38 <christophe.naud@gmail.com>
2024-12-19 17:17:48 +0100

    Create first real tests

--- tests/php-unit-tests/iTopModelInTuneCollectorTest.php
+++ tests/php-unit-tests/iTopModelInTuneCollectorTest.php
@@ -3,6 +3,7 @@
 namespace UnitTestFiles\Test;
 
 use iTopModelInTuneCollector;
+use utils;
 
 require_once (__DIR__.'/AbstractCollectorTestCase.php');
 require_once(APPROOT.'/core/parameters.class.inc.php');
@@ -16,10 +17,27 @@ require_once(APPROOT.'/collectors/src/InTuneCollectionPlan.class.inc.php');
 
 class iTopModelInTuneCollectorTest extends AbstractCollectorTestCase
 {
-    function testToto() {
-        $oiTopModelInTuneCollector = new iTopModelInTuneCollector;
-        $oiTopModelInTuneCollector->Init();
-        $this->assertTrue(true);
+    const string WRONG_MODEL_UNKNOWN_TYPE = 'NoUnknownConst';
+    private iTopModelInTuneCollector $oiTopModelInTuneCollector;
+    private string $sUnknownType;
+
+    public function setUp(): void
+    {
+        parent::Setup();
+
+        $this->oiTopModelInTuneCollector = new iTopModelInTuneCollector;
+        $this->oiTopModelInTuneCollector->Init();
+
+        $this->sUnknownType = Utils::GetConfigurationValue('model_unknown_type', self::WRONG_MODEL_UNKNOWN_TYPE);
+    }
+
+    function testIsModelUnknownTypeSet() {
+        $this->assertFalse($this->sUnknownType == self::WRONG_MODEL_UNKNOWN_TYPE);
+    }
+
+    function testGetTypeWithUnknownOS() {
+        $sType = $this->InvokeNonPublicMethod('iTopModelInTuneCollector', 'GetType', $this->oiTopModelInTuneCollector, ['MyOwnOS', '']);
+        $this->assertTrue($sType == $this->sUnknownType);
     }
 
 }
