7ab7f6d192feb5b1a57d447bad1c20da6d03048a
xtophe38 <christophe.naud@gmail.com>
2024-12-31 11:48:12 +0100

    Create tests for OS Families collector

---
+++ tests/php-unit-tests/iTopOSFamilyInTuneCollectorTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace UnitTestFiles\Test;
+
+use InTuneCollectionPlan;
+use iTopOSFamilyInTuneCollector;
+
+require_once (__DIR__.'/AbstractCollectorTestCase.php');
+require_once(APPROOT.'/core/parameters.class.inc.php');
+require_once(APPROOT.'/core/utils.class.inc.php');
+require_once(APPROOT.'/core/collector.class.inc.php');
+require_once(APPROOT.'/core/orchestrator.class.inc.php');
+require_once(APPROOT.'/core/jsoncollector.class.inc.php');
+require_once(APPROOT.'/collectors/src/iTopOSFamilyInTuneCollector.class.inc.php');
+require_once(APPROOT.'/core/collectionplan.class.inc.php');
+require_once(APPROOT.'/collectors/src/InTuneCollectionPlan.class.inc.php');
+
+class iTopOSFamilyInTuneCollectorTest extends AbstractCollectorTestCase
+{
+    private iTopOSFamilyInTuneCollector $oiTopOSFamilyInTuneCollector;
+
+    public function setUp(): void
+    {
+        // Initialize collection plan
+        $oCollectionPlan = new InTuneCollectionPlan();
+        $oCollectionPlan->MockInit();
+
+        parent::Setup();
+        $this->oiTopOSFamilyInTuneCollector = new iTopOSFamilyInTuneCollector;
+        $this->oiTopOSFamilyInTuneCollector->Init();
+    }
+
+    function testCollectFromForgedJson() {
+        // Copy forged json to data directory
+        $sJsonFile = APPROOT."collectors/tests/php-unit-tests/InTuneManagedDevices.json";
+        $bRes = copy($sJsonFile, $this->sDataPath.basename($sJsonFile));
+        if (!$bRes) {
+            throw new \Exception("Failed copying $sJsonFile to ".$this->sDataPath.basename($sJsonFile));
+        }
+
+        // Run collect
+        $this->assertTrue($this->oiTopOSFamilyInTuneCollector->Collect());
+
+        // Compare output csv
+        $sExpected_content = file_get_contents(APPROOT."/collectors/tests/php-unit-tests/expected_osfamily.csv");
+        $this->assertEquals($sExpected_content, file_get_contents(APPROOT."/data/iTopOSFamilyInTuneCollector-1.csv"));
+    }
+
+}
